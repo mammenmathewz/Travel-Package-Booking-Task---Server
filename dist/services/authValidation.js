56f0c88b88803a4d7258f2e3bf68a70643856f3a
Mammen Mathew <mammen999@gmail.com>
2025-04-25 03:37:34 +0530

    refactor: remove dist directory and its contents

--- dist/services/authValidation.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidEmailAndPassword = void 0;
-const isValidEmailAndPassword = (email, password) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const passwordRegex = /^(?=.*\d)(?=.*[^A-Za-z0-9]).{6,}$/;
-    return emailRegex.test(email.trim()) && passwordRegex.test(password.trim());
-};
-exports.isValidEmailAndPassword = isValidEmailAndPassword;
