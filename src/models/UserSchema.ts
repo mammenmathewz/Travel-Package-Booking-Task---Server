f8f85624621f4650ee47557579852d3bbf7b40ce
Mammen Mathew <mammen999@gmail.com>
2025-04-25 02:04:18 +0530

    fuser schema properties updated

--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -4,12 +4,12 @@ import { Schema, model } from "mongoose";
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String }, 
+  password: { type: String },
   profilePic: { type: String },
   address: { type: String },
+  phone: { type: String }, 
   isGoogleUser: { type: Boolean, default: false },
   role: { type: String, enum: ["admin", "user"], default: "user" },
 }, { timestamps: true });
 
 export const User = model("User", userSchema);
-
