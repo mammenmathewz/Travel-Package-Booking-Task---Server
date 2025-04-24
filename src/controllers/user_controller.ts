4ad906b28589ded947b397b6007f6827f7363d98
Mammen Mathew <mammen999@gmail.com>
2025-04-25 02:25:33 +0530

    add update profile functionality

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -23,3 +23,30 @@ export const getProfile = async (req: Request, res: Response) => {
   }
 };
 
+export const updateProfile = async (req: Request, res: Response) => {
+    const userId = req.params.userId;
+    const { name, address, phone, profilePic } = req.body;
+  
+    try {
+      const user = await User.findByIdAndUpdate(
+        userId,
+        { name, address, phone, profilePic },
+        { new: true } 
+      ).select("name address phone profilePic");
+  
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      res.status(200).json({
+        name: user.name,
+        address: user.address,
+        phone: user.phone,
+        profilePic: user.profilePic,
+      });
+    } catch (error) {
+      console.error("Error updating user profile:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+
--- src/routes/user_router.ts
+++ src/routes/user_router.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getProfile } from '../controllers/user_controller';
+import { getProfile, updateProfile } from '../controllers/user_controller';
 
 const userRouter = express.Router();
 
 userRouter.get('/profile/:userId', getProfile);
+userRouter.put('/update/:userId', updateProfile ); 
+
 
 export default userRouter;
